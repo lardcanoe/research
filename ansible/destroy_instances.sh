58fb5e34bca4a68ede89974ff8ef9a7294b18738
lardcanoe <lardcanoe@gmail.com>
2012-12-20 10:27:19 -0500

    Rearranged some code. DB gets setup as well.

--- ansible/destroy_instances.sh
+++ ansible/destroy_instances.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if [[ -z "$1" ]]; then
- echo >&2 "Please specify a name for the group."
- exit 1
+	echo >&2 "Usage: $0 <group name>"
+	exit 1
 fi
 
 ansible-playbook playbooks/terminate_instances.yml --extra-vars "grp_name=$1" -v
 
-echo "Waiting 30s for instances to terminate..."
-sleep 30
+if [[ "$2" != "-s" ]]; then
+	echo "Waiting 30s for instances to terminate..."
+	sleep 30
+fi
 
 ansible-playbook -i /etc/ansible/hosts ./playbooks/destroy_instances.yml --extra-vars "grp_name=$1"
--- ansible/provision_instances.sh
+++ ansible/provision_instances.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
- echo >&2 "Please specify a name for the new group."
+if [ -z "$1" ] || [ -z "$2" ]; then
+ echo >&2 "Usage: $0 <group name> <deployment type>"
  exit 1
 fi
 
-if [[ "$2" =~ ^dev|staging|production ]];
+if [ "$2" != "dev" ] && [ "$2" != "staging" ] && [ "$2" != "production" ];
 then
- echo >&2 "Please specify a deployment type: dev, staging, or production."
+ echo >&2 "Please specify a valid deployment type: dev, staging, or production."
  exit 1
 fi
+
+echo "Group Name: $1"
+echo "Deployment Type: $2"
 
 gname=$1
 deployment=$2
@@ -25,3 +28,5 @@ ansible-playbook ./playbooks/create_tags.yml --private-key "$privkey" --extra-va
 ansible-playbook ./playbooks/app_setup.yml --private-key "$privkey" --extra-vars "grp_name=$gname deployment=$deployment"
 
 ansible-playbook ./playbooks/db_setup.yml --private-key "$privkey" --extra-vars "grp_name=$gname deployment=$deployment"
+
+ansible-playbook ./playbooks/myapp_setup.yml --private-key "$privkey" --extra-vars "grp_name=$gname deployment=$deployment"
